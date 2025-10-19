52dad24155ca9efcc6d3fc3f1eaf36b78fa13ce8
tamirlan-bit <ktimor@gmail.com>
2025-10-18 21:44:45 -0700

    Evetything works

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,27 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :posts, dependent: :destroy
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
 
+  has_many :posts, dependent: :destroy
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h)
+        .where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }])
+        .first
+    else
+      where(conditions.to_h).first
+    end
+  end
 end
